34c77d6ebacec6335ffe4f7fbb3f5d5269437ac7
duarte-pompeu <ist173008@lab14p8.rnl.ist.utl.pt>
2017-03-08 10:08:34 +0000

    lab 1

--- lab01/src/ist/pa/Main.java
+++ lab01/src/ist/pa/Main.java
@@ -1,13 +1,27 @@
 package ist.pa;
 
+import java.util.Scanner;
+import java.lang.reflect.*;
+
 public class Main{
 	public static void main(String[] args){
 		
-		HelloWorld m1 = new HelloWorld();
-		m1.say();
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Enter your bff class:");
+		String className = reader.next();
 		
-		GoodbyeWorld m2 = new GoodbyeWorld();
-		m2.say();
+		try{
+			Class<?> myClass = Class.forName(className);
+			Constructor<?> constructor = myClass.getConstructor();
+			Object object = constructor.newInstance(new Object[] { });
+			
+			//get method that takes a String as argument
+			Method method = myClass.getMethod("say", null);
+			method.invoke(object, null);
+		}
 		
+		catch(Exception e){
+			System.out.println(":(");
+		}
 	}
 }
