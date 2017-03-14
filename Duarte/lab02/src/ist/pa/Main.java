f0c63737ef23f10f33d04c07144298630397a0e6
duarte-pompeu <pnt12@live.com.pt>
2017-03-14 22:51:44 +0000

    allow methods with many params, kind of

--- Duarte/lab02/src/ist/pa/Main.java
+++ Duarte/lab02/src/ist/pa/Main.java
@@ -5,10 +5,11 @@ import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main{
 	public static HashMap<String, Object> objectHashMap = new HashMap<String, Object>();
-	public static Object lastObject = null;
+	public static Object lastObject = "Undercover string";
 	public static void main(String[] args){
 		
 		Scanner reader = new Scanner(System.in);
@@ -59,23 +60,26 @@ public class Main{
 					
 					else if(commands.length > 1){
 						// has args
+						
 						ArrayList<Method> matchedMethods = getMatchingMethods(c, commands[0]);
 						
 						if( matchedMethods.size() == 0){
 							throw new RuntimeException("Error: no method found!");
 						}
 						else{
-							String param1 = commands[1];
-							Object o = tryCast(param1);
+							String[] stringParams = Arrays.copyOfRange(commands, 1, commands.length);
+							Object[] params = castParams(stringParams);
 							
 							for(Method m: matchedMethods){
 								try{
-									Object result = m.invoke(lastObject, o);
+									Object result = m.invoke(lastObject, params);
+									
 									System.out.println(result);
 									break;
 								}
 								catch (Exception e) {}
 							}
+							throw new RuntimeException("Matched methods couldn't execute process those parameters.\nFIXME: this error is always thrown");
 						}
 					}
 				}
@@ -91,11 +95,13 @@ public class Main{
 	}
 
 	static public Object tryCast(String stringObject) throws RuntimeException {
-		Class[] classes = { Double.class, Integer.class, String.class };
+		
+		Class[] classes = { Double.class, Integer.class, Character.class, String.class };
 		for(int i = 0; i < classes.length; i++){
 			try{
 				Class aClass = classes[i];
 				Object o = aClass.cast(stringObject);
+				System.out.println("".format("%s is a %s", o, aClass.getName()));
 				return o;
 			}
 			catch (Exception e){
@@ -114,5 +120,16 @@ public class Main{
 			}
 		}
 		return matchedMethods;
+	}
+	
+	static public Object [] castParams(String[] params){
+		Object [] castedObjects = new Object[params.length];
+		for(int i = 0; i < params.length; i++){
+			String s = params[i];
+			
+			Object o = tryCast(s);
+			castedObjects[i] = o;
+		}
+		return castedObjects;
 	}
 }
