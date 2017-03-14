2fe7b2f9edcbd27d29574c1d74d8349af9d3d0b3
duarte-pompeu <pnt12@live.com.pt>
2017-03-14 19:59:29 +0000

    stub for methods with params

--- Duarte/lab02/src/ist/pa/Main.java
+++ Duarte/lab02/src/ist/pa/Main.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main{
 	public static HashMap<String, Object> objectHashMap = new HashMap<String, Object>();
@@ -15,12 +16,13 @@ public class Main{
 		
 
 		String[] commands = null;
-		try{
-			while(true){
+		while(true){
+			String commandStr = "";
+			try {
 				userInput = reader.nextLine();
 				
 				commands = userInput.split(" ");
-				String commandStr = commands[0];
+				commandStr = commands[0];
 				Command c;
 				
 				if(commandStr.equalsIgnoreCase("Exit")){
@@ -29,31 +31,60 @@ public class Main{
 				
 				// get Command object
 				Class<?> myClass;
-				try {
-					myClass = Class.forName("ist.pa." + commandStr + "Command");
-					Constructor<?> constructor = myClass.getConstructor(String.class);
-					String argument = commands[1];
-					Object object = constructor.newInstance(new Object[] { argument });
+			
+				myClass = Class.forName("ist.pa." + commandStr + "Command");
+				Constructor<?> constructor = myClass.getConstructor(String.class);
+				String argument = commands[1];
+				Object object = constructor.newInstance(new Object[] { argument });
+				
+				Method method = myClass.getMethod("run", null);
+				method.invoke(object, null);
+				System.out.println(lastObject);
+			}
+			catch (ClassNotFoundException e1) {
+				try{
+					Class c = lastObject.getClass();
 					
-					Method method = myClass.getMethod("run", null);
-					method.invoke(object, null);
-					System.out.println(lastObject);
-				} 
-				catch (ClassNotFoundException e) {
-					System.out.println("Class not found.");
-					String invokeCommand = commandStr;
-					Method method = lastObject.getClass().getMethod(commandStr, null);
-					Object result = method.invoke(lastObject, null);
+					if(commands.length == 1){
+						// has no args
 					
-					if(result != null){
-						System.out.println(result);
+						String invokeCommand = commandStr;
+						Method method = c.getMethod(commandStr, null);
+						Object result = method.invoke(lastObject, null);
+						
+						if(result != null){
+							System.out.println(result);
+						}
 					}
+					
+					else if(commands.length > 1){
+						// has args
+						Method[] methods = c.getDeclaredMethods();
+						ArrayList<Method> matchedMethods = new ArrayList<Method>();
+						for(Method m: methods){
+							if(m.getName().equals(commandStr)){
+								matchedMethods.add(m);
+								System.out.println(m);
+							}
+						}
+						
+						Class[] classes = { Double.class, Integer.class, String.class };
+						int a = Integer.valueOf(commands[1]);
+						
+						
+						if( matchedMethods.size() == 0){
+							System.out.println("Error: no method found!");
+						}
+					}
+				}
+				catch (Exception e2){
+					System.out.println("Error: " + e2 + ": " + e2.getCause());
 				}
 				
 			}
-		}
-		catch(Exception e){
-			e.printStackTrace();
+			catch (Exception e){
+				System.out.println("Error: " + e + ": " + e.getCause());
+			}
 		}
 	}
 }
