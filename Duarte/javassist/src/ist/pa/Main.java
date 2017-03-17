b5dc5af23447fe3ef55fe1a4e6ff0a77106ef0a0
duarte-pompeu <ist173008@lab14p8.rnl.ist.utl.pt>
2017-03-17 16:45:50 +0000

    use separate classes, stil does not work

--- Duarte/javassist/src/ist/pa/Main.java
+++ Duarte/javassist/src/ist/pa/Main.java
@@ -5,31 +5,39 @@ import java.io.*;
 import java.lang.reflect.*;
 
 public class Main{
+	
+	
 	public static void main(String[] args) throws Exception{
-		ClassPool pool = ClassPool.getDefault();
-		pool.insertClassPath(new ClassClassPath(ist.pa.Main.class));
-		CtClass ctClass = pool.get("ist.pa.Main");
+
+	}
+	
+	public static void javassistTest() throws Exception{
+		Class c = Test.class;
+		String funcName = "function";	
 		
-		CtMethod ctMethod = ctClass.getDeclaredMethod("function");
+		
+		ClassPool pool = ClassPool.getDefault();
+		pool.insertClassPath(new ClassClassPath(c));
+		
+		String className = c.getCanonicalName();
+		System.out.println(className);
+		CtClass ctClass = pool.get(className);
+		
+		CtMethod ctMethod = ctClass.getDeclaredMethod(funcName);
 		String name = ctMethod.getName();
 		ctMethod.setName(name + "$original");
 		ctMethod = CtNewMethod.copy(ctMethod, name, ctClass, null);
 		ctMethod.setBody("{System.out.println(\"Goodbye\");}"
 			);
 		ctClass.addMethod(ctMethod);
-		
 		ctClass.writeFile();
-		Main.class.getMethod("function").invoke(null);
+		
+		c.getMethod("function").invoke(null);
 		for(Method m: Main.class.getMethods()){
 			if(m.getName().equals("function")){
 				m.invoke(null);
 			}
 		}
-		
-		function();
-	}
-	
-	public static void function(){
-		System.out.println("Hello.");
+		Test.function();
 	}
 }
---
+++ Duarte/javassist/src/ist/pa/Test.java
@@ -0,0 +1,8 @@
+package ist.pa;
+
+public class Test{
+	public static void function(){
+		System.out.println("Hello.");
+	}
+}
+
